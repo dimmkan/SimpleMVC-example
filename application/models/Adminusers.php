0020e3009ee73052527185a9469afa981d0da9a0
Dmitry Kanaev <dimmkan@mail.ru>
2019-11-30 15:12:09 +0300

    Практическое задание №11

--- application/models/Adminusers.php
+++ application/models/Adminusers.php
@@ -88,18 +88,24 @@ class Adminusers extends BaseExampleModel
     */
     public function update()
     {
-        $sql = "UPDATE $this->tableName SET timestamp=:timestamp, login=:login, pass=:pass, role=:role, email=:email  WHERE id = :id";  
-        $st = $this->pdo->prepare ( $sql );
-        $st->bindValue( ":timestamp", (new \DateTime('NOW'))->format('Y-m-d H:i:s'), \PDO::PARAM_STMT);
+        if (empty($this->pass)){
+            $sql = "UPDATE $this->tableName SET timestamp=:timestamp, login=:login, role=:role, email=:email  WHERE id =:id";
+        } else {
+            $sql = "UPDATE $this->tableName SET timestamp=:timestamp, login=:login, pass=:pass, role=:role, email=:email, salt=:salt  WHERE id =:id";
+        }
+        $st = $this->pdo->prepare( $sql );
+        $st->bindValue( ":timestamp", (new \DateTime('now'))->format('Y-m-d H:i:s'), \PDO::PARAM_STMT);
         $st->bindValue( ":login", $this->login, \PDO::PARAM_STR );
-        
-        // Хеширование пароля
-        $this->salt = rand(0,1000000);
-        $st->bindValue( ":salt", $this->salt, \PDO::PARAM_STR );
-        $this->pass .= $this->salt;
-        $hashPass = password_hash($this->pass, PASSWORD_BCRYPT);
-        $st->bindValue( ":pass", $hashPass, \PDO::PARAM_STR );
-        
+
+        if(!empty($this->pass)) {
+            // Хеширование пароля
+            $this->salt = rand(0, 1000000);
+            $st->bindValue(":salt", $this->salt, \PDO::PARAM_STR);
+            $this->pass .= $this->salt;
+            $hashPass = password_hash($this->pass, PASSWORD_BCRYPT);
+            $st->bindValue(":pass", $hashPass, \PDO::PARAM_STR);
+        }
+
         $st->bindValue( ":role", $this->role, \PDO::PARAM_STR );
         $st->bindValue( ":email", $this->email, \PDO::PARAM_STR );
         $st->bindValue( ":id", $this->id, \PDO::PARAM_INT );
--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -18,13 +18,21 @@ $User = Config::getObject('core.user.class');
 
 <form id="editUser" method="post" action="<?= $Url::link("admin/adminusers/edit&id=" . $_GET['id'])?>">
     <h5>Введите имя пользователя</h5>
-    <input type="text" name="login" placeholder="логин пользователя" value="<?= $viewAdminusers->login ?>"><br>
+    <input type="text" class="form-control" name="login" placeholder="логин пользователя" value="<?= $viewAdminusers->login ?>"><br>
     <h5>Введите пароль</h5>
-    <input type="text" name="pass" placeholder="новый пароль" value=""><br>
+    <input type="text" class="form-control" name="pass" placeholder="новый пароль" value=""><br>
     <h5>Введите e-mail</h5>
-    <input type="text" name="email"  placeholder="email" value="<?= $viewAdminusers->email ?>"><br>
-    
+    <input type="text" class="form-control" name="email"  placeholder="email" value="<?= $viewAdminusers->email ?>"><br>
+    <!-- Добавили вывод роли -->
+    <h5>Выберите роль пользователя</h5>
+    <select name="role" id="role" class="form-control">
+        <option <?php echo (($viewAdminusers->role == "admin") ? "selected" : ""); ?> value="admin">Администратор</option>
+        <option <?php echo (($viewAdminusers->role == "auth_user") ? "selected" : ""); ?> value="auth_user">Зарегистрированный пользователь</option>
+    </select>
+
+    <br>
+
     <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
-    <input type="submit" name="saveChanges" value="Сохранить">
-    <input type="submit" name="cancel" value="Назад">
+    <input type="submit" class="btn btn-primary" name="saveChanges" value="Сохранить">
+    <input type="submit" class="btn" name="cancel" value="Назад">
 </form>
